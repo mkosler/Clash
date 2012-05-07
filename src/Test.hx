959afe594a44af42e96ac4e5ec9835397826a080
Michael Kosler <marekkpie@gmail.com>
2012-05-06 22:15:52 -0500

    Added textinput

--- src/Test.hx
+++ src/Test.hx
@@ -6,6 +6,7 @@ import com.haxepunk.HXP;
 import clash.Button;
 import clash.Checkbox;
 import clash.RadioButton;
+import clash.TextInput;
 
 class Test extends World 
 {
@@ -23,6 +24,9 @@ class Test extends World
 		add(new RadioButton(10, 150, group1, "Number 1!"));
 		add(new RadioButton(10, 175, group1, "Number 2!"));
 		add(new RadioButton(10, 200, group1, "Number 3!"));
+
+		add(new TextInput(HXP.halfWidth, 100, false, "Single line!"));
+		add(new TextInput(HXP.halfWidth, 200, true, "Multiline!"));
 	}
 
 	private function ButtonCallback() : Void
---
+++ src/clash/TextInput.hx
@@ -0,0 +1,93 @@
+package clash;
+
+import com.haxepunk.Entity;
+import com.haxepunk.HXP;
+import com.haxepunk.graphics.Text;
+import com.haxepunk.utils.Input;
+import com.haxepunk.utils.Key;
+import nme.events.KeyboardEvent;
+
+class TextInput extends Entity 
+{
+	public var text(get_text, set_text) : String;
+	private var _text : String;
+	public function get_text() : String { return _text; }
+	public function set_text(text : String) : String
+	{
+		_text = text;
+		textGraphic.text = text;
+
+		setHitbox(textGraphic.textWidth, textGraphic.textHeight);
+
+		return text;
+	}
+
+	private var textGraphic : Text;
+	private var _multiline : Bool;
+	public static var focus : TextInput;
+
+	public function new(x : Float = 0, y : Float = 0, multiline : Bool = false, text : String = "")
+	{
+		super(x, y);
+
+		textGraphic = new Text(text);
+		textGraphic.resizable = true;
+		this.text = text;
+		_multiline = multiline;
+		graphic = textGraphic;
+
+		type = "textinput";
+	}
+
+	public override function update() : Void
+	{
+		super.update();
+
+		if (TextInput.focus == this) {
+			textGraphic.color = 0xC4DCF4;
+		} else {
+			textGraphic.color = 0xFFFFFF;
+		}
+
+		if (Input.mousePressed) {
+			if (collidePoint(x, y, world.mouseX, world.mouseY)) {
+				TextInput.focus = this;
+				Input.keyString = "";
+			} else if (world.collidePoint("textinput", world.mouseX, world.mouseY) == null) {
+				TextInput.focus = null;
+			}
+		}
+
+		if (TextInput.focus == this && Input.keyString != "") {
+			this.text += Input.keyString;
+			Input.keyString = "";
+		}
+	}
+
+	public override function added() : Void
+	{
+		super.added();
+
+		HXP.stage.addEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+	}
+
+	private function onKeyDown(e : KeyboardEvent) : Void
+	{
+		if (world != HXP.world) return;
+		if (TextInput.focus != this) return;
+		
+		switch (e.keyCode) {
+			case Key.BACKSPACE:
+				text = _text.substr(0, _text.length - 1);
+			case Key.ENTER:
+				if (_multiline) text += '\n';
+		}
+	}
+
+	public override function removed() : Void
+	{
+		super.removed();
+
+		HXP.stage.removeEventListener(KeyboardEvent.KEY_DOWN, onKeyDown);
+	}
+}
